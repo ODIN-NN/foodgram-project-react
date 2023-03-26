b7296dc3dbe17e108a90eeeea7c56eee4920e2e2
Alexander_Odintsov <odintsov.alexander.nn@yandex.com>
2023-03-26 23:32:21 +0300

    deploy27

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2d)3(qa6-%*oj33qxddz-5&!4f7!0!8*0-gc2gtjdpvslcfkz+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '62.84.117.39']
 
