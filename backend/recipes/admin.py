46e7c7d9ea6b8d6bb3225af8c2793032f7fb9f13
Alexander_Odintsov <odintsov.alexander.nn@yandex.com>
2023-03-28 16:11:13 +0300

    deploy38

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 
 from .models import (
-    Favorite, Ingredient, Recipe, ShoppingCart, Tag, IngredientRecipe
+    Favorite,
+    Ingredient,
+    IngredientRecipe,
+    Recipe,
+    ShoppingCart,
+    Tag,
 )
 
 
